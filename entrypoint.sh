6a895b24250d876f047f815f73691afd4aae9aad
Robert Kozak <robert.kozak@chromeriver.com>
2020-09-09 18:46:14 -0700

    Action changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ shift
 
 [[ "$ACTION"  == "create" ]] && add-branch $@ && exit 0
 [[ "$ACTION"  == "remove" ]] && remove-branch $@ && exit 0
-[[ "$ACTION"  == "deploy" ]] && deploy-service $@ && exit 0
+[[ "$ACTION"  == "pull_request" ]] && deploy-service $@ && exit 0
